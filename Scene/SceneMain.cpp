ea590ca4966fd734d73e6f886ae55c3431a4bf67
yukino sasa <ykn15wrkng@gmail.com>
2026-08-28 23:43:44 +0900

    [Add]Story
    ストーリー追加中

--- Scene/SceneMain.cpp
+++ Scene/SceneMain.cpp
@@ -22,7 +22,7 @@ namespace
 }
 
 SceneMain::SceneMain()
-	:m_current_stage_num(2), m_is_story(false), m_is_played_intro(false),
+	:m_current_stage_num(1), m_is_story(false), m_is_played_intro(false),
 	m_is_config(false), m_is_pause(false), m_is_goal(false)
 {
 	m_p_player = std::make_shared<Player>(m_game_data.m_spawn_pos);
@@ -140,6 +140,16 @@ void SceneMain::Update(GameSetting& game_setting, std::shared_ptr<SoundManager> 
 
 			m_is_played_intro = true;
 		}
+	}
+
+	if (m_p_player->GetPlayerCollectItem() == 10)
+	{
+		m_story_manager.PlayStory(StoryManager::Story::One);
+	}
+
+	if (m_p_player->GetPlayerCollectItem() == 35)
+	{
+		m_story_manager.PlayStory(StoryManager::Story::Two);
 	}
 
 	if (m_story_manager.GetIsPlayingStory())
